cb87ce7175cabc9408cdd9f3611c3e446e063aca
riyajtamboli <tambolir97@gmail.com>
2026-08-24 20:04:57 +0530

    Optimize sum pair solution

--- _01Arrays/_16SumPairOptimized.java
+++ _01Arrays/_16SumPairOptimized.java
@@ -1,5 +1,22 @@
+import java.util.HashSet;
+
 public class _16SumPairOptimized {
     void main(){
-        
+        int arr [] = {2, 4, 3, 5, 7, 8, -1};  
+        findPair(7, arr);
+    }
+
+    public static void findPair(int sum, int arr[]){
+        HashSet<Integer> hs = new HashSet<>();
+
+        for(int num : arr){
+            int needed = sum - num;
+
+            if(hs.contains(needed)){
+                System.out.println("(" + needed +", "+num +")");
+            }
+
+            hs.add(num);
+        }
     }
 }
